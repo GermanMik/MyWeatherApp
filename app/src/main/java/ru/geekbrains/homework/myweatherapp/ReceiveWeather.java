c89cb8ae624f9459393b595e475d723e52f7a314
GermanMik <tiktak@yandex.ru>
2017-09-04 01:25:39 +0300

    Added Screen with intent action

---
+++ app/src/main/java/ru/geekbrains/homework/myweatherapp/ReceiveWeather.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.homework.myweatherapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReceiveWeather extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receive_weather);
+    }
+}
