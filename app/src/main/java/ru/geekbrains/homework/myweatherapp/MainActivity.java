fee652136768c28de9f65ff8e02f48e5cabf2ffe
GermanMik <tiktak@yandex.ru>
2017-09-06 14:13:00 +0300

    Shared pref and saved instance added

--- app/src/main/java/ru/geekbrains/homework/myweatherapp/MainActivity.java
+++ app/src/main/java/ru/geekbrains/homework/myweatherapp/MainActivity.java
@@ -1,57 +1,85 @@
 package ru.geekbrains.homework.myweatherapp;
 
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.media.effect.Effect;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
 import android.support.annotation.StringDef;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.util.Map;
+import java.util.Set;
+
+import static ru.geekbrains.homework.myweatherapp.R.string.list_of_cities;
+
 public class MainActivity extends AppCompatActivity {
     private Button button;
-    private Button choose_weather;
-    private TextView effect;
-    private Spinner spinner;
     private Spinner spinner_of_cities;
-    private ColourEffect colourEffect = new ColourEffect();
+    private TextView textView;
     private Cities cities = new Cities();
-    private TextView weather_strings;
+    int spinnerPosition = 1;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        effect = (TextView) findViewById(R.id.effect);
-        spinner = (Spinner) findViewById(R.id.spinner);
+        if (savedInstanceState != null) {
+            spinner_of_cities.setSelection(spinnerPosition);
+        }
         spinner_of_cities = (Spinner) findViewById(R.id.spinner_of_cities);
-        button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.weather_result);
+        button = (Button) findViewById(R.id.choose_weather);
         button.setOnClickListener(new View.OnClickListener() {
 
-
             @Override
             public void onClick(View v) {
-                String colour = String.valueOf(spinner.getSelectedItem());
-                String result = colourEffect.getEffect(colour);
-                effect.setText(result);
-            }
-        });
-        choose_weather = (Button) findViewById(R.id.choose_weather);
-        choose_weather.setOnClickListener(new View.OnClickListener() {
 
-
-            @Override
-            public void onClick(View v) {
-                String list_of_cities = String.valueOf(spinner_of_cities.getSelectedItem());
-                String result = cities.getWeather(list_of_cities);
-                effect.setText(result);
+                sendMessage();
             }
         });
 
 
     }
 
+    private void sendMessage() {
+        String list_of_cities = String.valueOf(spinner_of_cities.getSelectedItem());
+        String result = cities.getWeather(getBaseContext(), list_of_cities);
+        Intent intent = new Intent(this, ReceiveWeather.class);
+        intent.putExtra(ReceiveWeather.RECEIVE_ACTIVITY_STRING, result);
+        startActivity(intent);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences sPref = getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sPref.edit();
+        int spinnerPosition = spinner_of_cities.getSelectedItemPosition();
+        editor.putInt("spinnerChoice",spinnerPosition);//Не могу понять как указать значение города для схранения в префы
+        editor.apply();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        int spinnerPosition = spinner_of_cities.getSelectedItemPosition();
+        outState.putInt("spinnerIndex", spinnerPosition);
+        Log.d("SpinnerState", "Spinner at position " + spinnerPosition + " was saved");
+
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState){
+        super.onRestoreInstanceState(savedInstanceState);
+        spinnerPosition = savedInstanceState.getInt("spinnerIndex");
+        Log.d("SpinnerState", "Spinner at position " + spinnerPosition + " was restored");}//не пойму как привильно восстановить значение, по логам нет ничего
 }
--- app/src/main/java/ru/geekbrains/homework/myweatherapp/ReceiveWeather.java
+++ app/src/main/java/ru/geekbrains/homework/myweatherapp/ReceiveWeather.java
@@ -1,13 +1,36 @@
 package ru.geekbrains.homework.myweatherapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ReceiveWeather extends AppCompatActivity {
+    public final static String RECEIVE_ACTIVITY_STRING = "intent";
+    private String temperature;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receive_weather);
+        if (savedInstanceState!=null){
+            temperature=savedInstanceState.getString("temperature");
+        }
+        Intent intent = getIntent();
+        if (intent != null) {
+            String message = intent.getStringExtra(RECEIVE_ACTIVITY_STRING);
+            TextView textView = (TextView) findViewById(R.id.textview_receiver);
+            textView.setText(message);
+        }
+    }
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Intent intent = getIntent();
+        if (intent != null) {
+            String temperature = intent.getStringExtra(RECEIVE_ACTIVITY_STRING);
+            outState.putString("temperature",temperature);
+        }
     }
 }
